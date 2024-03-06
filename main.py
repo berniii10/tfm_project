b180c71d4c9c326ba42226682074770065e9d903
Bernat Pujol <bernat.pujol@keysight.com>
2024-03-06 17:02:24 +0100

    Added the queries for IoT and PSU. Also added the first steps to DB connection but not tested yet

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import mysql.connector
+import queries
+
+
+
+def connectToDb():
+    myDb = mysql.connector.connect(user='admin', password='alliot',
+        host='alliot-kontrondev.alb.is.keysight.com',
+        database='postgres')
+
+    cursor = myDb.cursor()
+
+    cursor.close()
+    myDb.close()
+    print("Hello World")
+
+def myMain():
+    print(queries.iot_query)
+
+if __name__ == "__main__":
+    myMain()
